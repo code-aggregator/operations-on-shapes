7858d67f1fe490a75f974451bd523ba78d70090b
Max <maxim.shvets@gmail.com>
2017-08-20 14:04:32 +0300

    Wrote implementation for a function that returns bounding rectangle of a line segment

--- src/boundingRectangles.js
+++ src/boundingRectangles.js
@@ -1,3 +1,8 @@
-export function lineSegmentBoundingRect ([start, end]) {
-    return false
+import { point } from "./constructors"
+
+export function lineSegmentBoundingRect ([{x, y}, {x:x1, y:y1}]) {
+    return [
+        point(Math.min(x, x1), Math.min(y, y1)),
+        point(Math.max(x, x1), Math.max(y, y1))
+    ]
 }

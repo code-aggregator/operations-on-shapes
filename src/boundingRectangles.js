fdcb9da85ff69714335da316ca3954dc65db84e2
Max <maxim.shvets@gmail.com>
2017-08-25 14:22:34 +0300

    Created a function that returns bounding rectangle for an arbitrary shape, given that a shape's type is recognized. Rewrote tests for line segnemt's bounding rectangle to use this function.

--- src/boundingRectangles.js
+++ src/boundingRectangles.js
@@ -1,8 +1,16 @@
 import { point } from "./constructors"
 
-export function lineSegmentBoundingRect ([{x, y}, {x:x1, y:y1}]) {
+function lineSegmentBoundingRect ([{x, y}, {x:x1, y:y1}]) {
     return [
         point(Math.min(x, x1), Math.min(y, y1)),
         point(Math.max(x, x1), Math.max(y, y1))
     ]
+}
+
+let shapeBoundingRectFunc = {
+    lineSegment: lineSegmentBoundingRect
+};
+
+export function boundingRect (shape) {
+    return shapeBoundingRectFunc[shape.type](shape.representation);
 }
--- test/boundingRectangles.test.js
+++ test/boundingRectangles.test.js
@@ -1,20 +1,22 @@
 import { assert } from "chai";
-import { lineSegmentBoundingRect } from "../src/boundingRectangles";
-import { point } from "../src/constructors";
+import { boundingRect } from "../src/boundingRectangles";
+import { point, lineSegment } from "../src/constructors";
 
-describe("lineSegmentBoundingRect", function () {
-    it("returns a minimal rectangle that contains given line segment", function () {
-        assert.deepEqual(
-            lineSegmentBoundingRect([point(1, 3), point(5, 4)]),
-            [{x: 1, y: 3}, {x:5, y:4}]
-        );
-        assert.deepEqual(
-            lineSegmentBoundingRect([point(6, 7), point(2, 1)]),
-            [{x:2, y:1}, {x:6, y:7}]
-        );
-        assert.deepEqual(
-            lineSegmentBoundingRect([point(2, 6), point(5, 1)]),
-            [{x:2, y:1}, {x:5, y:6}]
-        )
-    })
+describe("boundingRect", function () {
+    describe("#lineSegment", function () {
+        it("returns a minimal rectangle that contains given line segment", function () {
+            assert.deepEqual(
+                boundingRect(lineSegment(point(1, 3), point(5, 4))),
+                [{x: 1, y: 3}, {x:5, y:4}]
+            );
+            assert.deepEqual(
+                boundingRect(lineSegment(point(6, 7), point(2, 1))),
+                [{x:2, y:1}, {x:6, y:7}]
+            );
+            assert.deepEqual(
+                boundingRect(lineSegment(point(2, 6), point(5, 1))),
+                [{x:2, y:1}, {x:5, y:6}]
+            )
+        })
+    });
 });

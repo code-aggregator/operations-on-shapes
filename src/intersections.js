574ba808f6d690a8a645a28839b2b0061541e841
Max <maxim.shvets@gmail.com>
2017-08-27 14:10:10 +0300

    Correction to line segments intersection check implementation. Removed misleading comment. Removed unused import.

--- src/intersections.js
+++ src/intersections.js
@@ -1,4 +1,3 @@
-import { point, pointInBasis } from "./constructors";
 import { boundingRect } from "./boundingRectangles";
 
 export function segmentsIntersect ([x1, y1], [x2, y2]) {
@@ -13,16 +12,6 @@ export function rectanglesIntersect ([upLeft1, bottomRight1], [upLeft2, bottomRi
 function pointInSegment (p, [x, y]) {
     return Math.min(x, y) <= p <= Math.max(x, y)
 }
-
-/**
- * Checks if two line segments intersect
- * Expects that bounding rectangles of each segment intersect
- * @param start1
- * @param end1
- * @param start2
- * @param end2
- * @returns {boolean}
- */
 
 function lineSegmentsIntersect ([start1, end1], [start2, end2]) {
     let deltaX1 = end1.x - start1.x;
@@ -39,7 +28,7 @@ function lineSegmentsIntersect ([start1, end1], [start2, end2]) {
         return c1 === c2
     }
 
-    let intersectionPointY = deltaY2 * c1 - deltaY1 * c2;
+    let intersectionPointY = (deltaY2 * c1 - deltaY1 * c2)/determinant;
 
     return pointInSegment(intersectionPointY, [start1.y, end1.y]) &&
         pointInSegment(intersectionPointY, [start2.y, end2.y])
